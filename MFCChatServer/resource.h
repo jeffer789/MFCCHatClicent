83cf329e14ae99ee6a18d05e11c01054eb18686c
ieffer789 <2419542331@qq.com>
2020-01-26 15:54:13 +0800

    添加例外的项目

---
+++ MFCChatServer/resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCChatServer.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCCHATSERVER_DIALOG        102
+#define IDR_MAINFRAME                   128
+#define IDC_STOP_BUTTON                 1000
+#define IDC_CAL_BUTTON                  1001
+#define IDC_MAIL_BUTTON                 1002
+#define IDC_STATAT_BUTTON               1003
+#define IDC_QQ_BUTTON                   1004
+#define IDC_CLEARMSG_BTN                1005
+#define IDC_SENT_BTN                    1006
+#define IDC_POND_EDIT                   1007
+#define IDC_SENT_EDIT                   1008
+#define IDC_MOSG_LIST                   1009
+#define IDC_COLOER_COMBO                1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
